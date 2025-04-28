ff21c32c5f84098772606d8ce84d6173e684d0d4
kennylin894 <Kennylin122004@gmail.com>
2025-04-28 10:36:47 -0400

    finished displaying all transactions in the ledger menu

--- src/main/java/com/ps/HelperMethods.java
+++ src/main/java/com/ps/HelperMethods.java
@@ -16,81 +16,87 @@ public class HelperMethods {
         }
     };
 
-        public static void ledgerMenuOptions() {
-            String ledgerMenu;
-            System.out.println("Welcome to the ledger");
-            System.out.println("=====================");
-            do {
-                System.out.println("Please choose an option:");
-                System.out.println("[A] All - (Display all enteries)");
-                System.out.println("[D] Deposits - (Display only the entries that are deposits into the account");
-                System.out.println("[P] Payments - (Display only the negative entries or payments)");
-                System.out.println("[R] Reports - (Search for reports)");
-                System.out.println("[H] Home - (Return Home)");
-                ledgerMenu = Main.stringScanner.nextLine();
-                if (ledgerMenu.equalsIgnoreCase("a")) {
-
-                } else if (ledgerMenu.equalsIgnoreCase("d")) {
-
-                } else if (ledgerMenu.equalsIgnoreCase("p")) {
-
-                } else if (ledgerMenu.equalsIgnoreCase("r")) {
-
-                } else {
-                    System.out.println("Error, bad input try again.");
+    public static void ledgerMenuOptions() {
+        String ledgerMenu;
+        System.out.println("Welcome to the ledger");
+        System.out.println("=====================");
+        do {
+            System.out.println("Please choose an option:");
+            System.out.println("[A] All - (Display all enteries)");
+            System.out.println("[D] Deposits - (Display only the entries that are deposits into the account");
+            System.out.println("[P] Payments - (Display only the negative entries or payments)");
+            System.out.println("[R] Reports - (Search for reports)");
+            System.out.println("[H] Home - (Return Home)");
+            ledgerMenu = Main.stringScanner.nextLine();
+            if (ledgerMenu.equalsIgnoreCase("a")) {
+                System.out.println("These are all the transactions. (Newest on top)");
+                System.out.println("===============================================");
+                for(Transaction transaction: readTransactionFile())
+                {
+                    System.out.println(toFormatData(transaction));
                 }
-            } while (!ledgerMenu.equals("H"));
-        }
+                System.out.println();
+            } else if (ledgerMenu.equalsIgnoreCase("d")) {
 
-        public static void addDepositMenuOptions() {
+            } else if (ledgerMenu.equalsIgnoreCase("p")) {
 
-        }
+            } else if (ledgerMenu.equalsIgnoreCase("r")) {
 
-        public static void makePaymentMenuOptions() {
-
-        }
-
-        public static ArrayList<Transaction> readTransactionFile() {
-            ArrayList<Transaction> listOfTransactions = new ArrayList<>();
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader("transaction.csv"));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] parts = line.split("\\|");
-                    LocalDate date = LocalDate.parse(parts[0]);
-                    LocalTime time = LocalTime.parse(parts[1]);
-                    String description = parts[2];
-                    String vendor = parts[3];
-                    double amount = Double.parseDouble(parts[4]);
-                    listOfTransactions.add(new Transaction(date, time, description, vendor, amount));
-                }
-                reader.close();
-            } catch (Exception e) {
-                System.out.println("File doesnt exist");
+            } else {
+                System.out.println("Error, bad input try again.");
             }
-            listOfTransactions.sort(comparator);
-            return listOfTransactions;
-        }
+        } while (!ledgerMenu.equals("H"));
+    }
 
-        public void writeToFile(Transaction transaction) {
-            try {
-                FileWriter writer = new FileWriter("transaction.csv", true);
-                BufferedWriter bufferedWriter = new BufferedWriter(writer);
-                bufferedWriter.write(toFormatData(transaction));
-                bufferedWriter.newLine();
-                bufferedWriter.close();
-            } catch (IOException e) {
-                System.out.println("Error writing to file.");
+    public static void addDepositMenuOptions() {
+
+    }
+
+    public static void makePaymentMenuOptions() {
+
+    }
+
+    public static ArrayList<Transaction> readTransactionFile() {
+        ArrayList<Transaction> listOfTransactions = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("transaction.csv"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                LocalDate date = LocalDate.parse(parts[0]);
+                LocalTime time = LocalTime.parse(parts[1]);
+                String description = parts[2];
+                String vendor = parts[3];
+                double amount = Double.parseDouble(parts[4]);
+                listOfTransactions.add(new Transaction(date, time, description, vendor, amount));
             }
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("File doesnt exist");
         }
+        listOfTransactions.sort(comparator);
+        return listOfTransactions;
+    }
 
-        public static String toFormatData(Transaction transaction) {
-            StringBuilder data = new StringBuilder();
-            data.append(transaction.getDate()).append(" ");
-            data.append(transaction.getTime()).append(" | ");
-            data.append(transaction.getDescription()).append(" | ");
-            data.append(transaction.getVendor()).append(" | $");
-            data.append(transaction.getAmount());
-            return data.toString();
+    public void writeToFile(Transaction transaction) {
+        try {
+            FileWriter writer = new FileWriter("transaction.csv", true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write(toFormatData(transaction));
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error writing to file.");
         }
+    }
+
+    public static String toFormatData(Transaction transaction) {
+        StringBuilder data = new StringBuilder();
+        data.append(transaction.getDate()).append(" ");
+        data.append(transaction.getTime()).append(" | ");
+        data.append(transaction.getDescription()).append(" | ");
+        data.append(transaction.getVendor()).append(" | $");
+        data.append(transaction.getAmount());
+        return data.toString();
+    }
 }
