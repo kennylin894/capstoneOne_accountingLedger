6f5fdd1c7c6969c943664aacfde69e39cbe0bce4
kennylin894 <Kennylin122004@gmail.com>
2025-04-30 13:24:08 -0400

    deleted some unused classes

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -2,8 +2,7 @@ package com.ps;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     static Scanner intScanner = new Scanner(System.in);
     static Scanner stringScanner = new Scanner(System.in);
